d028a2a0ebd2402f412c7ce1496b7627dece367a
Rayan JJ <randomhex1998@gmail.com>
2023-04-12 20:07:18 +0430

    add style to bottom box in dsk

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,16 +6,29 @@ const Main = () => {
     <div className="main-area" style={{ backgroundImage: `url(${TehranImg})` }}>
       <div className="container">
         <div className="search-input">
-          <input type="text" className="search" placeholder="Enter Your Location ..." />
+          <input
+            type="text"
+            className="search"
+            placeholder="Enter Your Location ..."
+          />
         </div>
         <div className="weth-title-area">
           <p className="weth-title">Tehran</p>
           <h2 className="weth-temp">15 F</h2>
         </div>
-        <div className="bottom-box">
-          <div className="Feels Like">15</div>
-          <div className="Humidity">49</div>
-          <div className="Wind Speed">123 MPH</div>
+        <div className="bottom-box-area">
+          <div className="bottom-box FeelsLike">
+            <h4>15</h4>
+            <p>FeelsLike</p>
+          </div>
+          <div className="bottom-box Humidity">
+            <h4>15</h4>
+            <p>Humidity</p>
+          </div>
+          <div className="bottom-box WindSpeed">
+            <h4>6 MPH</h4>
+            <p>WindSpeed</p>
+          </div>
         </div>
       </div>
     </div>
