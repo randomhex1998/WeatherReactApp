df28cb4e6602754d39721393a8e7298e16638b3f
Rayan JJ <randomhex1998@gmail.com>
2023-04-12 19:47:08 +0430

    make search area and input

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,7 +6,7 @@ const Main = () => {
     <div className="main-area" style={{ backgroundImage: `url(${TehranImg})` }}>
       <div className="container">
         <div className="search-input">
-          <input type="text" className="search" />
+          <input type="text" className="search" placeholder="Enter Your Location ..." />
         </div>
         <div className="weth-title-area">
           <p className="weth-title">Tehran</p>
