128375817713e3484ac23aaf592b2db950d737f7
Rayan JJ <randomhex1998@gmail.com>
2023-04-12 21:53:41 +0430

    change bg with name

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,32 +1,45 @@
 import React, { useState } from "react";
 import axios from "axios";
-import TehranImg from "../asset/Tehran.jpg";
+import sunSet from "../asset/sunset.jpg";
 
 const Main = () => {
-  const URL =
-    "https://api.openweathermap.org/data/2.5/weather?q=London&appid=58a2d482587b71e599e2ec23c971e402";
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
+
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=58a2d482587b71e599e2ec23c971e402`;
 
   const SearchHandler = (event) => {
     if (event.key === "Enter") {
       axios.get(URL).then((response) => {
+        setData(response.data);
         console.log(response.data);
       });
     }
   };
+
   return (
-    <div className="main-area" style={{ backgroundImage: `url(${TehranImg})` }}>
+    <div
+      className="main-area"
+      style={{
+        backgroundImage: `${
+          data.name == location
+            ? `url(${require(`../asset/${location}.jpg`)}) `
+            : `url(${sunSet})`
+        }`,
+      }}>
       <div className="container">
         <div className="search-input">
           <input
             type="text"
             className="search"
             placeholder="Enter Your Location ..."
+            value={location}
+            onChange={(event) => setLocation(event.target.value)}
+            onKeyPress={SearchHandler}
           />
         </div>
         <div className="weth-title-area">
-          <p className="weth-title">Tehran</p>
+          <p className="weth-title">{data.name}</p>
           <h2 className="weth-temp">15 F</h2>
         </div>
         <div className="bottom-box-area">
