0b2328ec3e063c3797152e184f0866687f858054
Rayan JJ <randomhex1998@gmail.com>
2023-04-12 19:42:34 +0430

    make jsx backbone

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,20 @@ import TehranImg from "../asset/Tehran.jpg";
 const Main = () => {
   return (
     <div className="main-area" style={{ backgroundImage: `url(${TehranImg})` }}>
-      <div className="container">5555</div>
+      <div className="container">
+        <div className="search-input">
+          <input type="text" className="search" />
+        </div>
+        <div className="weth-title-area">
+          <p className="weth-title">Tehran</p>
+          <h2 className="weth-temp">15 F</h2>
+        </div>
+        <div className="bottom-box">
+          <div className="Feels Like">15</div>
+          <div className="Humidity">49</div>
+          <div className="Wind Speed">123 MPH</div>
+        </div>
+      </div>
     </div>
   );
 };
